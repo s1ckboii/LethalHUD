9d3808a78927965c1fc2dc0678f85e970910216b
pacoito123 <pacoito@protonmail.com>
2025-11-26 17:07:42 -0600

    Replaced some Reflection uses that can be handled by publicizing.

--- Compats/BetterScanVisionProxy.cs
+++ Compats/BetterScanVisionProxy.cs
@@ -1,15 +1,10 @@
-﻿using LethalHUD.Configs;
-using System;
-using System.Linq;
-using System.Reflection;
+﻿using BetterScanVision;
+using LethalHUD.Configs;
 using System.Runtime.CompilerServices;
-using UnityEngine;
 
 namespace LethalHUD.Compats;
 internal class BetterScanVisionProxy
 {
-    private static FieldInfo _nightVisionField;
-
     [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
     internal static void OverrideNightVisionColor()
     {
@@ -18,28 +13,7 @@ internal class BetterScanVisionProxy
         if (GameNetworkManager.Instance == null || GameNetworkManager.Instance.localPlayerController == null)
             return;
 
-        if (_nightVisionField == null)
-        {
-            var t = AppDomain.CurrentDomain.GetAssemblies()
-                .Select(a => a.GetType("BetterScanVision.PlayerControllerBPatches"))
-                .FirstOrDefault(x => x != null);
-
-            if (t == null)
-            {
-                Loggers.Warning("[BetterScanVisionProxy] Failed to find PlayerControllerBPatches type.");
-                return;
-            }
-
-            _nightVisionField = t.GetField("nightVision", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
-        }
-
-        if (_nightVisionField != null)
-        {
-            Light light = _nightVisionField.GetValue(null) as Light;
-            if (light != null)
-            {
-                light.color = ConfigHelper.GetScanColor();
-            }
-        }
+        if (PlayerControllerBPatches.nightVision != null)
+            PlayerControllerBPatches.nightVision.color = ConfigHelper.GetScanColor();
     }
 }
